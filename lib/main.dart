a8f663106f3b453bfad74e98fea8466a22e7c2fe
Christian Pino <christian.softwaredelevoper@gmail.com>
2024-05-22 00:25:43 -0500

    Nuevos cambios app peluditos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(const MyApp());
 
@@ -20,6 +22,7 @@ class MyApp extends StatelessWidget {
       title: '',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Calistoga',
       ),
       home: HomeScreen(),
     );
@@ -67,7 +70,7 @@ class _HomeScreenState extends State<HomeScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'EncuentraTuMascota',
+                  'Encuentra Tu Mascota',
                   style: TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
@@ -87,35 +90,236 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 0, 0, 0),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/patitas.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          children: [
-            Container(
-              height: 200.0,
-              child: PageView.builder(
-                controller: _pageController,
-                itemCount: sliderImages.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.0),
-                      image: DecorationImage(
-                        image: AssetImage(sliderImages[index]),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  );
-                },
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/patitas.jpg'),
+                fit: BoxFit.cover,
               ),
             ),
-          ],
+          ),
+          Column(
+            children: [
+              Container(
+                height: 200.0,
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemCount: sliderImages.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        image: DecorationImage(
+                          image: AssetImage(sliderImages[index]),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            left: 90,
+            top: MediaQuery.of(context).size.height / 2 - 40,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AdoptionFormScreen(),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+              ),
+              child: Text(
+                'Dar en adopción',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AdoptionFormScreen extends StatefulWidget {
+  @override
+  _AdoptionFormScreenState createState() => _AdoptionFormScreenState();
+}
+
+class _AdoptionFormScreenState extends State<AdoptionFormScreen> {
+  String? _selectedAnimalType;
+  XFile? _selectedImage;
+  String? _selectedCity; // Nuevo atributo para almacenar la ciudad seleccionada
+
+  final _imagePicker = ImagePicker();
+
+  void _getImage() async {
+    final XFile? pickedImage = await _imagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _selectedImage = pickedImage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Formulario de Adopción'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              DropdownButton<String>(
+                hint: Text('Selecciona el tipo de animal'),
+                onChanged: (String? value) {
+                  setState(() {
+                    _selectedAnimalType = value;
+                  });
+                },
+                value: _selectedAnimalType,
+                items: <String>['Perro', 'Gato'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 20),
+              DropdownButton<String>(
+                hint: Text('Selecciona la ciudad'), // Nuevo desplegable para la ciudad
+                onChanged: (String? value) {
+                  setState(() {
+                    _selectedCity = value;
+                  });
+                },
+                value: _selectedCity,
+                items: <String>['Bogotá', 'Cali', 'Pereira'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: _getImage,
+                child: Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: _selectedImage != null
+                          ? Image.file(
+                              File(_selectedImage!.path),
+                              fit: BoxFit.cover,
+                            )
+                          : Icon(Icons.add_a_photo, size: 50),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Agrega una foto del peludito',
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Número de contacto',
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Nombre del animal (Opcional)',
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                maxLines: null,
+                decoration: InputDecoration(
+                  hintText: 'Descripción del animal',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+                            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Lógica para guardar y publicar
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                    child: Text(
+                      'Guardar y Publicar',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                    child: Text(
+                      'Cancelar',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
